2e098ff8d822690d346a978951d8fe24953d9b44
computerfr33k <computerfreak@computerfr33k.com>
2018-12-31 15:32:33 -0600

    Add starter deploy script (bootstrap)

---
+++ ansible/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APULL="$(which ansible-pull)"
+MYREPO="https://github.com/computerfr33k/fedora_workstation_builder.git"
+
+# Bootstrap Ansible
+if rpm -q ansible
+then
+    echo "Ansible already installed"
+else
+    sudo dnf install ansible
+fi
+
+if [ ! -f $HOME/.ansible.cfg ]; then
+    touch $HOME/.ansible.cfg
+    echo '[defaults]' >$HOME/.ansible.cfg
+    echo 'remote_tmp     = /tmp/$USER/ansible' >>$HOME/.ansible.cfg
+fi
+
+sudo $APULL -U $MYREPO
